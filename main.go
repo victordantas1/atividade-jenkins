21ee925b951d22fce10124b54c9675599da85ef9
victordantas1 <victorr.dantas@gmail.com>
2026-05-27 18:33:52 -0400

    chore: add fail

--- main.go
+++ main.go
@@ -8,11 +8,11 @@ func CelsiusParaFahrenheit(c float64) float64 {
 }
 
 // FahrenheitParaCelsius converte F para C
-func FahrenheitParaCelsius(f float64) float64 {
+fun FahrenheitParaCelsius(f float64) float64 {
 	return (f - 32) * 5 / 9
 }
 
 func main() {
 	fmt.Printf("25°C em Fahrenheit é: %.2f°F\n", CelsiusParaFahrenheit(25))
-	fmt.Printf("77°F em Celsius é: %.2f°C\n", FahrenheitParaCelsius(77)
+	fmt.Printf("77°F em Celsius é: %.2f°C\n", FahrenheitParaCelsius(77))
 }
